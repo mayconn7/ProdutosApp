3b8537d18405ba74bb3d3ccc2a5cbb851ae6abcd
Maycon da Silva de Oliveira <mayconoliveira@fiotec.fiocruz.br>
2025-05-06 21:46:25 -0300

    feat(api): entidades de domínio, documentação da api e configuração de ambiente.

---
+++ ProdutosApp.Domain/Entities/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutosApp.Domain.Entities
+{
+    /// <summary>
+    /// Modelo de entidade de domínio para categoria
+    /// </summary>
+    public class Categoria
+    {
+        #region Propriedades
+        public Guid? Id { get; set; }
+        public string? Nome { get; set; }
+        public bool? Ativo { get; set; }
+        #endregion
+
+        #region Relacionamentos
+        public ICollection<Produto>? Produtos { get; set; }
+        #endregion
+
+    }
+}
---
+++ ProdutosApp.Domain/Entities/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutosApp.Domain.Entities
+{
+    /// <summary>
+    /// Modelo de entidade de domínio para produto
+    /// </summary>
+    public class Produto
+    {
+        #region Propriedades
+
+        
+        public Guid? Id { get; set; }
+        public string? Nome { get; set; }
+        public decimal? Preco { get; set; }
+        public int? Quantidade { get; set; }
+        public DateTime? DataHoraCriacao { get; set; }
+        public bool? Ativo { get; set; }
+        public Guid? CategoriaId { get; set; }
+        #endregion
+
+        #region Relacionamentos
+        public Categoria? Categoria { get; set; }
+        #endregion
+    }
+}
